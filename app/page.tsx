a8bfb64856b51f58d019045347524e6eee4e3823
Seth Ketsi <christambassador12@gmail.com>
2024-01-10 23:27:45 +0000

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1 className='text-3xl font-extrabold text-red-600'>Hello World</h1>
+    <h1 className='text-3xl'>Hello World</h1>
   )
 }
